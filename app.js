5316887fd9e3587a47c6ae021cf33117d9d673eb
Chase Wood <wood.c.chase@gmail.com>
2019-06-19 18:25:27 -0600

    Proxy routing fixed

--- app.js
+++ app.js
@@ -8,9 +8,59 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
+app.get('/descriptions', (req, res) => {
+  res.redirect('http://52.53.232.73:3003/descriptions')
+});
+
+app.get('/mensSizes', (req, res) => {
+  res.redirect('http://52.53.232.73:3003/mensSizes')
+});
+
+app.get('/womensSizes', (req, res) => {
+  res.redirect('http://52.53.232.73:3003/womensSizes')
+});
+
+app.get('/euSizes', (req, res) => {
+  res.redirect('http://52.53.232.73:3003/euSizes')
+});
+
+app.get('/reviews', (req, res) => {
+  res.redirect('http://13.57.29.73:3002/reviews')
+});
+
+
 app.listen(3333, (err) => {
   if (err) {
     return console.error(err);
   }
   console.log('listening on port 3333')
 })
+
+
+app.listen(3000, err => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('You are connected to the proxy on port 3000')
+});
+
+app.listen(3001, err => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('You are connected to the proxy on port 3001')
+});
+
+app.listen(3002, err => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('You are connected to the proxy on port 3002')
+});
+
+app.listen(3003, err => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('You are connected to the proxy on port 3003')
+});
